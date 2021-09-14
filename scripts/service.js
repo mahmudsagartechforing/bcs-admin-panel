80b4227e96278fa6a9dfd21f5a96d22b12648c1d
mahmudsagartechforing <mahmudsagar.techforing@gmail.com>
2021-09-14 15:47:30 +0600

    add subservice and edit subservice design changed

--- scripts/service.js
+++ scripts/service.js
@@ -13,9 +13,49 @@ $(document).ready(function () {
             sSearch: `_INPUT_ <i class="bi bi-search"></i>`,
             sSearchPlaceholder: "Search...",
         },
-        "initComplete": function(settings, json) {
+        "initComplete": function (settings, json) {
             $('body').find('.dataTables_scrollBody').addClass("scrollbar");
         },
         "paging": false,
     });
 })
+
+
+const forms = document.querySelectorAll('.needs-validation')
+forms.forEach((form)=>{
+    form.addEventListener("submit",(event)=>{
+        event.preventDefault()
+        if (!form.checkValidity()) {
+            event.stopPropagation()
+            console.log(form);
+          }
+  
+          form.classList.add('was-validated')
+          form.classList.add('d-none')
+          form.nextElementSibling.classList.remove("d-none")
+
+    })
+}, false)
+
+
+
+tinymce.init({
+    selector: '.title-container',
+    plugins: 'image code lists',
+    menubar: false,
+    statusbar: false,
+    toolbar: 'undo redo | image code | bold italic alignleft aligncenter alignright alignjustify| bullist numlist outdent indent',
+  
+    /* without images_upload_url set, Upload tab won't show up*/
+    images_upload_url: 'postAcceptor.php',
+  
+    /* we override default upload handler to simulate successful upload*/
+    images_upload_handler: function (blobInfo, success, failure) {
+      setTimeout(function () {
+        /* no matter what you upload, we will turn it into TinyMCE logo :)*/
+        success('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png');
+      }, 2000);
+    },
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
