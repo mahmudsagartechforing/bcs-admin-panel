c6b5e1bae2a2573c5a30c6ee7d2a7ad13ab23aa4
mahmudsagartechforing <mahmudsagar.techforing@gmail.com>
2021-09-19 18:23:26 +0600

    subscribed packages added

--- scripts/subscription.js
+++ scripts/subscription.js
@@ -1,3 +1,31 @@
+let packageChart = document.getElementById("package-chart").getContext("2d");
+
+let revenuePieChart = new Chart(packageChart, {
+    type: "pie",
+    data: {
+        labels: ["MAXIMUM", "BASIC", "PREMIUM", "MEDIUM"],
+        datasets: [{
+            label: "Subscribed client",
+            backgroundColor: ['#182F59','#5BBC2E','#FFD500', '#DC143C'],
+            data: [45, 25, 10, 20],
+        },  ],
+    },
+      
+    options: {
+        responsive: true,
+        legend: {
+            display: true,
+            labels: {
+                fontColor: "black",
+                boxWidth:15,
+            },
+            // maxWidth: 30,
+            position: "left",
+        },        
+    },
+});
+
+
 $(document).ready(function () {
     $("#subscription-list").DataTable({
         scrollCollapse: true,
