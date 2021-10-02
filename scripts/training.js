0a95831057305ef0bbdb51fcc8f872491a658add
mahmudsagartechforing <mahmudsagar.techforing@gmail.com>
2021-10-02 13:26:29 +0600

    training page complete

--- scripts/training.js
+++ scripts/training.js
@@ -1,22 +1,22 @@
-$(document).ready(function () {
-    $("#training-list").DataTable({
-        scrollCollapse: true,
-        info: false,
-        columnDefs: [{
-            orderable: false,
-            targets: -1
-        }],
-        sorting: false,
-        oLanguage: {
-            sSearch: `_INPUT_ <i class="bi bi-search"></i>`,
-            sSearchPlaceholder: "Search...",
-        },
-        "initComplete": function (settings, json) {
-            $('body').find('.dataTables_scrollBody').addClass("scrollbar");
-        },
-        "paging": false,
-    });
-})
+// $(document).ready(function () {
+//     $("#training-list").DataTable({
+//         scrollCollapse: true,
+//         info: false,
+//         columnDefs: [{
+//             orderable: false,
+//             targets: -1
+//         }],
+//         sorting: false,
+//         oLanguage: {
+//             sSearch: `_INPUT_ <i class="bi bi-search"></i>`,
+//             sSearchPlaceholder: "Search...",
+//         },
+//         "initComplete": function (settings, json) {
+//             $('body').find('.dataTables_scrollBody').addClass("scrollbar");
+//         },
+//         "paging": false,
+//     });
+// })
 
 tinymce.init({
     selector: '#longDescription',
@@ -38,3 +38,50 @@ tinymce.init({
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
 });
 
+FilePond.create(
+    document.querySelector('.filepond')
+);
+FilePond.create(
+    document.querySelector('.previewVideo')
+);
+var uppy = new Uppy.Core()
+.use(Uppy.Dashboard, {
+  inline: true,
+  target: '#drag-drop-area',
+  showProgressDetails: true,
+})
+.use(Uppy.Tus, {endpoint: 'https://tusd.tusdemo.net/files/'})
+
+uppy.on('complete', (result) => {
+console.log('Upload complete! We’ve uploaded these files:', result.successful)
+})
+
+
+
+const addMulti = document.querySelectorAll(".addMulti")
+const mainCourseAdd = document.querySelector(".mainCourseAdd")
+const courseSectionAdd = document.querySelector(".courseSectionAdd")
+const courseSectionModuleAdd = document.querySelector(".courseSectionModule .courseSectionAdd")
+const mainCourse = document.querySelector(".main-course")
+const courseSection = document.querySelector(".courseSection")
+const courseSectionModule = document.querySelector(".courseSectionModule")
+const formInputs = document.querySelectorAll("select, input, textarea")
+addMulti.forEach(add=>{
+    add.addEventListener("click", (e)=>{
+        e.preventDefault()
+        formInputs.forEach(input=>{
+            input.value = ""
+        })
+    })
+})
+
+mainCourseAdd.addEventListener("click", (e)=>{
+    e.preventDefault()
+    mainCourse.classList.add("d-none")
+    courseSection.classList.remove("d-none")
+})
+courseSectionAdd.addEventListener("click", (e)=>{
+    e.preventDefault()
+    courseSection.classList.add("d-none")
+    courseSectionModule.classList.remove("d-none")
+})
